a0d69b5ab86455d8da28c28b93b0085b2081db09
minyekim <biie2004@naver.com>
2024-09-27 17:03:36 +0900

    Add : test코드 추가

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,8 +4,13 @@
 #include "MateriaSource.hpp"
 #include "AMateria.hpp"
 
+// void leaks() {
+// 	system("leaks Materia");
+// }
+
 int main()
 {
+	// atexit(leaks);
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
@@ -15,9 +20,16 @@ int main()
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+	for (int i = 0; i < 3; i++) {
+		tmp = src->createMateria("ice");
+		me->equip(tmp);
+	}
 	ICharacter* bob = new Character("bob");
 	me->use(0, *bob);
 	me->use(1, *bob);
+	me->unequip(2);
+	me->unequip(2);
+	me->unequip(5);
 	delete bob;
 	delete me;
 	delete src;
