c8ac6db91897d1f5a13ba04a24ed63972364e218
minyekim <biie2004@naver.com>
2024-10-01 15:35:31 +0900

    Fix : Amateria *로 선언된 Ice끼리의 복사 연산자를 위해 복사 연산자를 순수 가상, public으로 변경

--- ex03/AMateria.hpp
+++ ex03/AMateria.hpp
@@ -10,14 +10,15 @@ class AMateria
 {
 private :
 	AMateria(const AMateria& other);
-	AMateria& operator=(const AMateria& other);
 protected:
 	std::string _type;
 public:
 	AMateria();
+	virtual AMateria& operator=(const AMateria& other) = 0;
 	AMateria(std::string const & type);
 	virtual ~AMateria();
-	std::string const & getType() const; //Returns the materia type
+
+	std::string const & getType() const;
 	virtual AMateria* clone() const = 0;
 	virtual void use(ICharacter& target);
 };
