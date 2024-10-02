8d9b34056afee2dbf4f7db3d7fd7b183fdbb5116
minyekim <biie2004@naver.com>
2024-10-02 18:12:37 +0900

    Refactor : 명시적으론 interface이기 때무에 occf 제거

--- ex03/ICharacter.hpp
+++ ex03/ICharacter.hpp
@@ -5,12 +5,8 @@
 
 class ICharacter
 {
-private :
-	ICharacter(const ICharacter& other);
-	ICharacter& operator=(const ICharacter& other);
 public:
-	ICharacter();
-	virtual ~ICharacter();
+	virtual ~ICharacter() {}
 	virtual std::string const & getName() const = 0;
 	virtual void equip(AMateria* m) = 0;
 	virtual void unequip(int idx) = 0;
--- ex03/IMateriaSource.hpp
+++ ex03/IMateriaSource.hpp
@@ -7,12 +7,8 @@
 
 class IMateriaSource
 {
-private:
-	IMateriaSource(const IMateriaSource& other);
-	IMateriaSource& operator=(const IMateriaSource& other);
 public:
-	IMateriaSource();
-	virtual ~IMateriaSource();
+	virtual ~IMateriaSource() {}
 	virtual void learnMateria(AMateria*) = 0;
 	virtual AMateria* createMateria(std::string const & type) = 0;
 };
