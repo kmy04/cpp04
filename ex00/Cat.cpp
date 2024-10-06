06452510666df0d28f4f955a6e0a2f5049d3a06e
minyekim <biie2004@naver.com>
2024-10-06 19:23:24 +0900

    Refactor : 생성자 문구 대문자를 소문자로

--- ex00/Cat.cpp
+++ ex00/Cat.cpp
@@ -3,24 +3,24 @@
 Cat::Cat() 
 : Animal("Cat")
 {
-	std::cout << "Cat Default constructor called" << std::endl;
+	std::cout << "Cat default constructor called" << std::endl;
 }
 
 Cat::Cat(const Cat& other) {
 	_type = other._type;
-	std::cout << "Cat Copy constructor called" << std::endl;
+	std::cout << "Cat copy constructor called" << std::endl;
 }
 
 Cat& Cat::operator=(const Cat& other) {
 	if (this != &other) {
 		_type = other._type;
-		std::cout << "Cat Copy assignment operator called" << std::endl;
+		std::cout << "Cat copy assignment operator called" << std::endl;
 	}
 	return (*this);
 }
 
 Cat::~Cat() {
-	std::cout << "Cat Destructor called" << std::endl;
+	std::cout << "Cat destructor called" << std::endl;
 }
 
 void Cat::makeSound() const {
--- ex00/Dog.cpp
+++ ex00/Dog.cpp
@@ -3,23 +3,23 @@
 Dog::Dog() 
 : Animal("Dog")
 {
-	std::cout << "Dog Default constructor called" << std::endl;
+	std::cout << "Dog default constructor called" << std::endl;
 }
 
 Dog::Dog(const Dog& other) {
 	_type = other._type;
-	std::cout << "Dog Copy constructor called" << std::endl;
+	std::cout << "Dog copy constructor called" << std::endl;
 }
 
 Dog& Dog::operator=(const Dog& other) {
 	if (this != &other) {
 		_type = other._type;
-		std::cout << "Dog Copy assignment operator called" << std::endl;
+		std::cout << "Dog copy assignment operator called" << std::endl;
 	}
 	return (*this);}
 
 Dog::~Dog() {
-	std::cout << "Dog Destructor called" << std::endl;
+	std::cout << "Dog destructor called" << std::endl;
 }
 
 void Dog::makeSound() const {
